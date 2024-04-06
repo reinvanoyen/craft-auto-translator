74daa9360508e8b55011f20408919017e7967236
Rein Van Oyen <rein.vanoyen@littlemissrobot.com>
2024-04-06 06:15:49 +0200

    improvement: updated the DefaultPolicy with some real rules

--- src/Policies/DefaultPolicy.php
+++ src/Policies/DefaultPolicy.php
@@ -5,6 +5,7 @@ namespace Lmr\AutoTranslator\Policies;
 use craft\elements\Entry;
 use craft\helpers\ElementHelper;
 use Lmr\AutoTranslator\Contracts\Policy;
+use Lmr\AutoTranslator\Plugin;
 
 class DefaultPolicy implements Policy
 {
@@ -14,6 +15,22 @@ class DefaultPolicy implements Policy
      */
     public function shouldTranslate(Entry $entry): bool
     {
+        $config = Plugin::getInstance()->getSettings();
+        $fromLanguages = $config->fromLanguages;
+
+        // The language we've just edited and need to translate from
+        $currentLanguage = $entry->site->language;
+
+        // Only translate when the current language is in the list of "from languages"
+        if (! in_array($currentLanguage, $fromLanguages)) {
+            return false;
+        }
+
+        // Only translate when the entry is enabled for the site
+        if (! $entry->enabledForSite) {
+            return false;
+        }
+
         return ! (ElementHelper::isDraftOrRevision($entry) || $entry->resaving);
     }
 }
