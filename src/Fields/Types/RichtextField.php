eb71125857619fcaf924216ccc57be95db608e01
Rein Van Oyen <rein.vanoyen@littlemissrobot.com>
2024-05-02 15:54:51 +0200

    fix: updated TextField & RichtextField to comply to the new FieldInterface

--- src/Fields/Types/RichtextField.php
+++ src/Fields/Types/RichtextField.php
@@ -4,27 +4,45 @@ namespace Lmr\AutoTranslator\Fields\Types;
 
 use craft\elements\Entry;
 use Lmr\AutoTranslator\Contracts\FieldInterface;
+use Lmr\AutoTranslator\Fields\Field;
 
-class RichtextFieldInterface implements FieldInterface
+class RichtextField extends Field implements FieldInterface
 {
     /**
-     * @param string $name
-     * @param Entry $entry
-     * @return string
+     * @param string $fromLanguage
+     * @param string $toLanguage
+     * @param Entry $translateEntry
+     * @return void
      */
-    public function get(string $name, Entry $entry): string
+    public function translate(string $fromLanguage, string $toLanguage, Entry $translateEntry): void
     {
-        return strip_tags($entry->{$name}->getRawContent());
+        // Get the original content for this field and entry
+        $content = $this->get();
+
+        // Translate it
+        $translatedContent = $this->service->translate($content, $fromLanguage, $toLanguage);
+
+        // Save!
+        $this->save($translateEntry, $translatedContent);
     }
 
     /**
-     * @param string $name
-     * @param Entry $entry
+     * @return string
+     */
+    private function get(): string
+    {
+        $field = $this->originalEntry->{$this->name};
+
+        return strip_tags($field->getRawContent());
+    }
+
+    /**
+     * @param Entry $translateEntry
      * @param string $contents
      * @return void
      */
-    public function save(string $name, Entry $entry, string $contents): void
+    private function save(Entry $translateEntry, string $contents): void
     {
-        $entry->{$name} = $contents;
+        $translateEntry->{$this->name} = $contents;
     }
 }
--- src/Fields/Types/TextField.php
+++ src/Fields/Types/TextField.php
@@ -4,27 +4,25 @@ namespace Lmr\AutoTranslator\Fields\Types;
 
 use craft\elements\Entry;
 use Lmr\AutoTranslator\Contracts\FieldInterface;
+use Lmr\AutoTranslator\Fields\Field;
 
-class TextFieldInterface implements FieldInterface
+class TextField extends Field implements FieldInterface
 {
     /**
-     * @param string $name
-     * @param Entry $entry
-     * @return string
-     */
-    public function get(string $name, Entry $entry): string
-    {
-        return $entry->{$name};
-    }
-
-    /**
-     * @param string $name
-     * @param Entry $entry
-     * @param string $contents
+     * @param string $fromLanguage
+     * @param string $toLanguage
+     * @param Entry $translateEntry
      * @return void
      */
-    public function save(string $name, Entry $entry, string $contents): void
+    public function translate(string $fromLanguage, string $toLanguage, Entry $translateEntry): void
     {
-        $entry->{$name} = $contents;
+        // Get the original content for this field and entry
+        $content = $this->originalEntry->{$this->name};
+
+        // Translate it
+        $translatedContent = $this->service->translate($content, $fromLanguage, $toLanguage);
+
+        // Save!
+        $translateEntry->{$this->name} = $translatedContent;
     }
 }
